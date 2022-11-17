2d94f92596afc683fbc213009f0b865118ed9b49
jhzz <jakub.dyja@gmail.com>
2022-11-17 11:32:49 +0100

    Refactoring
    Removed nav meshes, renamed files,

--- Assets/TileGenerationExtended/CustomTileManager.cs
+++ Assets/TileGenerationExtended/CustomTileManager.cs
@@ -7,14 +7,8 @@ using UnityEngine.AI;
 
 public class CustomTileManager : TileManager<CustomTile>
 {
-    public delegate void NavMeshRebuildCallback (bool isStart);
-    public event NavMeshRebuildCallback OnNavMeshRebuild;
-
     [field: SerializeField]
     private List<CustomTile> TilePrefabsCollection { get; set; } = new List<CustomTile>();
-    [field: SerializeField]
-    private TileConnector TileConectorPrefab { get; set; }
-    private List<TileConnector> TileConnectors { get; set; } = new List<TileConnector>();
     [field: SerializeField]
     public NavMeshSurface NavMesh { get; private set; }
 
@@ -38,35 +32,6 @@ public class CustomTileManager : TileManager<CustomTile>
     private void HandleTileGenerated (CustomTile generatedTile)
     {
         generatedTile.GameobjectsContainer.rotation = GetRandomRotation();
-        Dictionary<WorldSide, CustomTile> sorroundingTiles = GetSorroundingTiles(generatedTile);
-
-        foreach (KeyValuePair<WorldSide, CustomTile> singleTile in sorroundingTiles)
-        {
-            if (DoesConnectorAlreadyExists(generatedTile, singleTile.Value) == false)
-            {
-                TileConnector createdConnector = Instantiate(TileConectorPrefab);
-                createdConnector.Initialize(generatedTile, singleTile.Value, singleTile.Key, GetReverseSide(singleTile.Key), SideSize);
-                TileConnectors.Add(createdConnector);
-            }
-        }
-    }
-
-    private bool DoesConnectorAlreadyExists (CustomTile firstTile, CustomTile secondTile)
-    {
-        bool output = false;
-
-        foreach (TileConnector singleConnector in TileConnectors)
-        {
-            output = singleConnector.IsThisConnectorConnectingTwoTiles(firstTile, secondTile);
-
-            if (output == true)
-            {
-                break;
-            }
-
-        }
-
-        return output;
     }
 
     private CustomTile GetRandomTile ()
